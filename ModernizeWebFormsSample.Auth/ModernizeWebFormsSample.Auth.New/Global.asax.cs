487a127ca0ae9335d02b2ecd6aa0b67b454c5f28
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-04-23 16:30:56 -0400

    x

--- ModernizeWebFormsSample.Auth/ModernizeWebFormsSample.Auth.New/Global.asax.cs
+++ ModernizeWebFormsSample.Auth/ModernizeWebFormsSample.Auth.New/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -12,16 +13,17 @@ namespace ModernizeWebFormsSample.Auth.New
         protected void Application_Start(object sender, EventArgs e)
         {
             // create default user
-//            var defaultUser = Membership.GetUserNameByEmail("test@user.local");
-//            if (defaultUser == null)
-//            {
-//                MembershipCreateStatus status;
-//                Membership.CreateUser("test@user.local", "Pa$$w0rd", "test@user.local", "question", "answer", true, out status);
-//                if (status != MembershipCreateStatus.Success)
-//                {
-//                    throw new Exception($"Couldn't create a default user! {status}");
-//                }
-//            }
+            //            var defaultUser = Membership.GetUserNameByEmail("test@user.local");
+            //            if (defaultUser == null)
+            //            {
+            //                MembershipCreateStatus status;
+            //                Membership.CreateUser("test@user.local", "Pa$$w0rd", "test@user.local", "question", "answer", true, out status);
+            //                if (status != MembershipCreateStatus.Success)
+            //                {
+            //                    throw new Exception($"Couldn't create a default user! {status}");
+            //                }
+            //            }
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
--- ModernizeWebFormsSample.Auth/ModernizeWebFormsSample.Auth.New/Startup.cs
+++ ModernizeWebFormsSample.Auth/ModernizeWebFormsSample.Auth.New/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Steeltoe.Security.Authentication.CloudFoundry.Owin;
 
 [assembly: OwinStartup(typeof(ModernizeWebFormsSample.Auth.New.Startup))]
 
@@ -12,10 +15,21 @@ namespace ModernizeWebFormsSample.Auth.New
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            ApplicationConfig.RegisterConfig("development");
+
+            app.SetDefaultSignInAsAuthenticationType("ExternalCookie");
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                LoginPath = new PathString("/Login.aspx")
+                AuthenticationType = "ExternalCookie",
+                AuthenticationMode = AuthenticationMode.Active,
+                CookieName = ".AspNet.ExternalCookie",
+                LoginPath = new PathString("/Account/AuthorizeSSO"),
+                ExpireTimeSpan = TimeSpan.FromMinutes(5)
             });
+
+            app.UseCloudFoundryOpenIdConnect(ApplicationConfig.Configuration, "CloudFoundry", ApplicationConfig.LoggerFactory);
+
+//            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
 }
